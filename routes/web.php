ddabbca24b5f8ee782860ba9438b216081e342cf
spforero <sandraforero1@unisangil.edu.co>
2022-02-18 22:22:04 -0500

    crear controladores de las vistas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdministradorController;
+use App\Http\Controllers\DocenteController;
+use App\Http\Controllers\EstudianteController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +18,6 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class);
+    
+
