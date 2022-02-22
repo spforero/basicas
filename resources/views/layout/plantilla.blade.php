184b68c656367d28167c6c35402cbb757f8b473b
spforero <sandraforero1@unisangil.edu.co>
2022-02-21 21:36:57 -0500

    cambios plantilla

--- resources/views/layout/plantilla.blade.php
+++ resources/views/layout/plantilla.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Qyield('title')</title>
+    <title>@yield('title')</title>
     <!--favicon-->
     <!--estilos-->
     <style>
@@ -41,13 +41,11 @@
     
     <!--nav-->
     
-<div>
-    <br>
-    <br>
-    <br>
-</div>
+
 
     @yield('content')
+
+    
     <!--footer-->
     <footer class=" cont p-4">
         <p class="text-center text-white font-serif font-bold">FUNDACIÓN UNIVERSITARIA DE SAN GIL - UNISANGIL SEDE CHIQUINQUIRÁ</p>
--- resources/views/modulos/index.blade.php
+++ resources/views/modulos/index.blade.php
@@ -1,11 +1,30 @@
 @extends('layout.plantilla')
 @section('title', 'historia')
 @section('content')
-<div class="content h-64 bg-red-200">
+<div class="cont bg-blue-900  flex ">
+    <div class="imagen bg-contain"></div>
+   <h1 class="text-6xl font-bold m-10 text-white font-serif ">BasicSciences</h1>
+   <a  class="text-xl font-bold my-20 ml-64 text-white font-serif object-right hover:text-blue-300" href="">Iniciar Sesion</a>
 
 </div>
 
+<div class="container h-full bg-blue-100 mt-4 mb-4 ml-10 rounded-xl shadow-2xl">
+    <h1 class="text-3xl font-bold text-center pt-4  mb-4">Bienvenidos a nuestra comunidad Unisangilista Queridos Compañeros</h1>
+   <img class="m-auto my-6" src="img/unisan.jpg" alt="">
+   <div class="text-justify mx-40" ><p >Hace dos décadas se creó en Colombia una institución de educación superior cuyas características la han hecho particularmente diferente: Unisangil. Su origen fue la Economía Solidaria. Nació en San Gil, Santander, en 1988, motivada
+       por el interés que tuvo un grupo de organizaciones sociales y cooperativas que vieron en la profesionalización, la ciencia, el conocimiento y la técnica un campo estratégico para el modelo de desarrollo regional.</p>
+   <p>Dando cumplimiento a su vocación de origen, Unisangil ha tenido un papel de primer orden en la promoción de microempresarios, como también en la consolidación de un capital humano de primer nivel para buscar respuestas a necesidades
+       y problemáticas de sus zonas de influencia, en un mapa que trasciende las fronteras físicas del departamento de Santander y se integra con Casanare y Boyacá. En 1992 inició labores académicas la sede San Gil; en 1996, la sede Yopal,
+       y en 2000, la sede Chiquinquirá.
+   </p>
+   <p>En 2000 Unisangil firmó una alianza de cooperación con la Universidad Autónoma de Bucaramanga, UNAB, gracias a ella se ha tenido un fortalecimiento institucional y académico, este último se traduce en la oferta de programas académicos
+       de la UNAB en la zona de influencia de Unisangil.
+   </p>
+   <br>
+</div>
+</div>
 
+ 
 
  
 @endsection
